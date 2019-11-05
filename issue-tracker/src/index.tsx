ca0f7a2cbbcc958df82ec8b3e2ea3d83255b64b6
Renan Machado <renan.mav@hotmail.com>
2019-11-05 12:41:06 -0300

    create RelayEnvironment

--- issue-tracker/src/index.tsx
+++ issue-tracker/src/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { RelayEnvironmentProvider } from 'react-relay/hooks'
 import './index.css'
+import RelayEnvironment from './RelayEnvironment'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <RelayEnvironmentProvider environment={RelayEnvironment}>
+    <App />
+  </RelayEnvironmentProvider>,
+  document.getElementById('root'),
+)
